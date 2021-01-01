e8a6833eaec7d0b6b777d4b9b35fc2e5c353eda9
Brutus5000 <Brutus5000@gmx.net>
2021-01-01 23:58:31 +0100

    Add i18n for forms

--- src/main/kotlin/com/faforever/usermanagement/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/faforever/usermanagement/config/SecurityConfiguration.kt
@@ -6,9 +6,6 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity
 // Import required as of https://youtrack.jetbrains.com/issue/KT-43578
 import org.springframework.security.config.web.server.invoke
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -34,16 +31,6 @@ class SecurityConfiguration {
             formLogin { disable() }
             logout { disable() }
         }
-
-    @Bean
-    fun userDetailsService(passwordEncoder: PasswordEncoder): MapReactiveUserDetailsService {
-        val user: UserDetails = User
-            .withUsername("user")
-            .password(passwordEncoder.encode("password"))
-            .roles("USER")
-            .build()
-        return MapReactiveUserDetailsService(user)
-    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
