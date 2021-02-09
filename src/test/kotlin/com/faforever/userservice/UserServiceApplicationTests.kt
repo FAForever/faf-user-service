a426b3861afb7da0227ab0060116fd97b6d395b0
Brutus5000 <Brutus5000@gmx.net>
2021-02-09 21:34:41 +0100

    Fix build

--- src/test/kotlin/com/faforever/userservice/UserServiceApplicationTests.kt
+++ src/test/kotlin/com/faforever/userservice/UserServiceApplicationTests.kt
@@ -2,9 +2,11 @@ package com.faforever.userservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-class UsermanagementApplicationTests {
+@ActiveProfiles("integration")
+class UserServiceApplicationTests {
 
     @Test
     fun contextLoads() {
