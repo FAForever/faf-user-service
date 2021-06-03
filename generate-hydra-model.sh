145c01f949c45d0a96d615cd05a8e58fe6808516
Brutus5000 <Brutus5000@gmx.net>
2021-06-03 11:09:22 +0200

    Update to Ory Hydra model to v1.10.2

--- generate-hydra-model.sh
+++ generate-hydra-model.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-HYDRA_VERSION="v1.9.2"
+HYDRA_VERSION="v1.10.2"
 
 mkdir -p hydra-generated
 
 docker run --user 1000 --rm -v "${PWD}/hydra-generated:/local" openapitools/openapi-generator-cli generate \
-    -i "https://raw.githubusercontent.com/ory/hydra/${HYDRA_VERSION}/.schema/api.swagger.json" \
+    -i "https://raw.githubusercontent.com/ory/hydra/${HYDRA_VERSION}/spec/api.json" \
     -g kotlin \
     --additional-properties modelPackage=sh.ory.hydra.model,serializationLibrary=jackson,swaggerAnnotations=false,hideGenerationTimestamp=true \
     -o /local
--- src/main/kotlin/sh/ory/hydra/model/LogoutRequest.kt
+++ src/main/kotlin/sh/ory/hydra/model/LogoutRequest.kt
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  *
+ * @param challenge Challenge is the identifier (\"logout challenge\") of the logout authentication request. It is used to identify the session.
  * @param requestUrl RequestURL is the original Logout URL requested.
  * @param rpInitiated RPInitiated is set to true if the request was initiated by a Relying Party (RP), also known as an OAuth 2.0 Client.
  * @param sid SessionID is the login session ID that was requested to log out.
@@ -22,6 +23,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
  */
 
 data class LogoutRequest(
+    /* Challenge is the identifier (\"logout challenge\") of the logout authentication request. It is used to identify the session. */
+    @field:JsonProperty("challenge")
+    val challenge: kotlin.String? = null,
     /* RequestURL is the original Logout URL requested. */
     @field:JsonProperty("request_url")
     val requestUrl: kotlin.String? = null,
