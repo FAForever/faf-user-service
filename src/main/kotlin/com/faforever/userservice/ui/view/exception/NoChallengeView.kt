cb977f65abc35a2e82054dd9ab76b5f6023ccf3e
Sheikah45 <Sheikah450@gmail.com>
2023-07-15 22:06:59 -0400

    Clean up

--- src/main/kotlin/com/faforever/userservice/ui/view/exception/NoChallengeView.kt
+++ src/main/kotlin/com/faforever/userservice/ui/view/exception/NoChallengeView.kt
@@ -1,4 +1,4 @@
-package com.faforever.userservice.ui.view
+package com.faforever.userservice.ui.view.exception
 
 import com.faforever.userservice.backend.hydra.NoChallengeException
 import com.faforever.userservice.backend.i18n.I18n
