9f7d5a21c4bdb2bb351843d2a7502096718004ec
Brutus5000 <Brutus5000@gmx.net>
2023-08-10 15:52:31 +0200

    Apply spotless formatting

--- src/main/kotlin/com/faforever/userservice/ui/view/registration/RegisterView.kt
+++ src/main/kotlin/com/faforever/userservice/ui/view/registration/RegisterView.kt
@@ -140,9 +140,11 @@ class RegisterView(
                 getTranslation("register.username.taken"),
             ).bind("username")
 
-        binder.forField(email).withValidator(EmailValidator(getTranslation("register.email.invalid"))).withValidator(
-            { email -> registrationService.emailAvailable(email) == EmailStatus.EMAIL_AVAILABLE },
-            getTranslation("register.email.taken"),
+        binder.forField(email)
+            .withValidator(EmailValidator(getTranslation("register.email.invalid")))
+            .withValidator(
+                { email -> registrationService.emailAvailable(email) == EmailStatus.EMAIL_AVAILABLE },
+                getTranslation("register.email.taken"),
         ).bind("email")
 
         binder.forField(termsOfService).asRequired(getTranslation("register.acknowledge.terms")).bind("termsOfService")
