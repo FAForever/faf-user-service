402358a655d5e3294d3470038a6bc1c7754d285f
Sheikah45 <Sheikah450@gmail.com>
2023-08-17 18:29:06 -0400

    Fix styling and text field responsiveness

--- src/main/kotlin/com/faforever/userservice/ui/view/registration/RegisterView.kt
+++ src/main/kotlin/com/faforever/userservice/ui/view/registration/RegisterView.kt
@@ -7,6 +7,7 @@ import com.faforever.userservice.config.FafProperties
 import com.faforever.userservice.ui.component.FafLogo
 import com.faforever.userservice.ui.component.SocialIcons
 import com.faforever.userservice.ui.layout.CardLayout
+import com.faforever.userservice.ui.layout.CompactHorizontalLayout
 import com.faforever.userservice.ui.layout.CompactVerticalLayout
 import com.vaadin.flow.component.Text
 import com.vaadin.flow.component.button.Button
@@ -22,6 +23,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.data.binder.Binder
 import com.vaadin.flow.data.validator.EmailValidator
+import com.vaadin.flow.data.value.ValueChangeMode
 import com.vaadin.flow.router.Route
 
 @Route("/register", layout = CardLayout::class)
@@ -40,19 +42,16 @@ class RegisterView(private val registrationService: RegistrationService, fafProp
 
     private val username = TextField(null, getTranslation("register.username")).apply {
         setWidthFull()
+        valueChangeMode = ValueChangeMode.LAZY
     }
     private val email = TextField(null, getTranslation("register.email")).apply {
         setWidthFull()
+        valueChangeMode = ValueChangeMode.LAZY
     }
-    private val termsOfService = Checkbox(false).apply {
-        setWidthFull()
-    }
-    private val privacyPolicy = Checkbox(false).apply {
-        setWidthFull()
-    }
-    private val rules = Checkbox(false).apply {
-        setWidthFull()
-    }
+    private val termsOfService = Checkbox(false)
+    private val privacyPolicy = Checkbox(false)
+    private val rules = Checkbox(false)
+
     private val submit = Button(getTranslation("register.action")) { register() }.apply {
         isEnabled = false
         addThemeVariants(ButtonVariant.LUMO_PRIMARY)
@@ -75,25 +74,31 @@ class RegisterView(private val registrationService: RegistrationService, fafProp
         add(formHeader)
 
         val readAndAgree = getTranslation("register.readAndAgree") + " "
-        val termsOfServiceLayout = HorizontalLayout(
+        val termsOfServiceLayout = CompactHorizontalLayout(
             termsOfService,
             Text(readAndAgree),
             Anchor(
                 fafProperties.account().registration().termsOfServiceUrl(),
                 getTranslation("register.termsOfService"),
-            ),
+            ).apply { addClassName("policy-link") },
         )
-        val privacyPolicyLayout = HorizontalLayout(
+        val privacyPolicyLayout = CompactHorizontalLayout(
             privacyPolicy,
             Text(
                 readAndAgree,
             ),
-            Anchor(fafProperties.account().registration().privacyStatementUrl(), getTranslation("register.privacy")),
+            Anchor(
+                fafProperties.account().registration().privacyStatementUrl(),
+                getTranslation("register.privacy")
+            ).apply { addClassName("policy-link") },
         )
-        val rulesLayout = HorizontalLayout(
+        val rulesLayout = CompactHorizontalLayout(
             rules,
             Text(readAndAgree),
-            Anchor(fafProperties.account().registration().rulesUrl(), getTranslation("register.rules")),
+            Anchor(
+                fafProperties.account().registration().rulesUrl(),
+                getTranslation("register.rules")
+            ).apply { addClassName("policy-link") },
         )
 
         add(username, email, termsOfServiceLayout, privacyPolicyLayout, rulesLayout, submit)
@@ -104,8 +109,7 @@ class RegisterView(private val registrationService: RegistrationService, fafProp
 
         add(footer)
 
-        binder.forField(username)
-            .asRequired(getTranslation("register.username.required"))
+        binder.forField(username).asRequired(getTranslation("register.username.required"))
             .withValidator({ username -> username[0].isLetter() }, getTranslation("register.username.startsWithLetter"))
             .withValidator({ username -> username.length in 3..15 }, getTranslation("register.username.size"))
             .withValidator(
@@ -116,9 +120,7 @@ class RegisterView(private val registrationService: RegistrationService, fafProp
                 getTranslation("register.username.taken"),
             ).bind("username")
 
-        binder.forField(email)
-            .withValidator(EmailValidator(getTranslation("register.email.invalid")))
-            .withValidator(
+        binder.forField(email).withValidator(EmailValidator(getTranslation("register.email.invalid"))).withValidator(
                 { email -> registrationService.emailAvailable(email) == EmailStatus.EMAIL_AVAILABLE },
                 getTranslation("register.email.taken"),
             ).bind("email")
