7e51aa1dbcda3743e903252a039f48a11856ab4b
Brutus5000 <Brutus5000@gmx.net>
2025-07-11 23:50:07 +0200

    Revert "Use websocket bidirectionally"
    This reverts commit 7316369d77e5e379bb4dc8f101e7b6deda2731ee.

--- src/main/kotlin/com/faforever/userservice/AppConfig.kt
+++ src/main/kotlin/com/faforever/userservice/AppConfig.kt
@@ -1,8 +1,6 @@
 package com.faforever.userservice
 
 import com.vaadin.flow.component.page.AppShellConfigurator
-import com.vaadin.flow.component.page.Push
-import com.vaadin.flow.shared.ui.Transport
 import com.vaadin.flow.theme.Theme
 import io.quarkus.runtime.StartupEvent
 import jakarta.enterprise.context.ApplicationScoped
@@ -10,7 +8,6 @@ import jakarta.enterprise.event.Observes
 
 @Theme("faforever")
 @ApplicationScoped
-@Push(transport = Transport.WEBSOCKET)
 class AppConfig : AppShellConfigurator {
     fun onStart(@Observes event: StartupEvent) {
         System.setProperty("vaadin.copilot.enable", "false")
