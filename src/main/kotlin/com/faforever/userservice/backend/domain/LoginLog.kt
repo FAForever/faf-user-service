ef0bcdae15b559a8b937cbbd9d7e5be0c40521c7
BlackYps <jonas-fb@gmx.de>
2025-11-14 01:10:36 +0100

    return correct last login time

--- src/main/kotlin/com/faforever/userservice/backend/domain/LoginLog.kt
+++ src/main/kotlin/com/faforever/userservice/backend/domain/LoginLog.kt
@@ -38,9 +38,12 @@ data class FailedAttemptsSummary(
 
 @ApplicationScoped
 class LoginLogRepository : PanacheRepository<LoginLog> {
-    fun findLastLoginTime(userId: Int?): LocalDateTime? =
-        find("userId = ?1", userId).firstResult()?.createTime
-    // or last result?
+    fun findLastLoginTime(userId: Int?): LocalDateTime? {
+        if (userId == null) {
+            return null
+        }
+        return find("userId = ?1 order by createTime desc", userId).firstResult()?.createTime
+    }
 
     fun findFailedAttemptsByIpAfterDate(ip: String, date: LocalDateTime): FailedAttemptsSummary? =
         getEntityManager().createQuery(
