4b6346b08f19eff3ef1a821e09783cca3ba98d7b
Brutus5000 <Brutus5000@gmx.net>
2025-11-14 21:10:53 +0100

    Update to Ory Hydra v25.4.0 (#357)

--- generate-hydra-model.sh
+++ generate-hydra-model.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-HYDRA_VERSION="v1.10.2"
+HYDRA_VERSION="v25.4.0"
+USER=1000
 
 mkdir -p hydra-generated
 
-docker run --user 1000 --rm -v "${PWD}/hydra-generated:/local" openapitools/openapi-generator-cli generate \
+docker run --rm -v "${PWD}/hydra-generated:/local" openapitools/openapi-generator-cli generate \
     -i "https://raw.githubusercontent.com/ory/hydra/${HYDRA_VERSION}/spec/api.json" \
     -g kotlin \
-    --additional-properties modelPackage=sh.ory.hydra.model,serializationLibrary=jackson,swaggerAnnotations=false,hideGenerationTimestamp=true \
+    --additional-properties modelPackage=sh.ory.hydra.model,serializationLibrary=jackson \
     -o /local
+
+sudo chown -R $USER:$USER hydra-generated
--- src/main/kotlin/com/faforever/userservice/backend/hydra/HydraClient.kt
+++ src/main/kotlin/com/faforever/userservice/backend/hydra/HydraClient.kt
@@ -14,14 +14,15 @@ import jakarta.ws.rs.QueryParam
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
-import sh.ory.hydra.model.AcceptConsentRequest
-import sh.ory.hydra.model.AcceptLoginRequest
-import sh.ory.hydra.model.ConsentRequest
-import sh.ory.hydra.model.GenericError
-import sh.ory.hydra.model.LoginRequest
-import sh.ory.hydra.model.OAuth2TokenIntrospection
+import sh.ory.hydra.model.AcceptOAuth2ConsentRequest
+import sh.ory.hydra.model.AcceptOAuth2LoginRequest
+import sh.ory.hydra.model.IntrospectedOAuth2Token
+import sh.ory.hydra.model.OAuth2ConsentRequest
+import sh.ory.hydra.model.OAuth2LoginRequest
+import sh.ory.hydra.model.OAuth2RedirectTo
+import sh.ory.hydra.model.RejectOAuth2Request
 
-@Path("/")
+@Path("/admin")
 @ApplicationScoped
 @RegisterRestClient(configKey = "faf-ory-hydra")
 interface HydraClient {
@@ -41,42 +42,42 @@ interface HydraClient {
     // requesting a handled challenge throws HTTP 410 - Gone
     @GET
     @Path("/oauth2/auth/requests/login")
-    fun getLoginRequest(@QueryParam("login_challenge") @NotBlank challenge: String): LoginRequest
+    fun getLoginRequest(@QueryParam("login_challenge") @NotBlank challenge: String): OAuth2LoginRequest
 
     @GET
     @Path("/oauth2/auth/requests/consent")
-    fun getConsentRequest(@QueryParam("consent_challenge") @NotBlank challenge: String): ConsentRequest
+    fun getConsentRequest(@QueryParam("consent_challenge") @NotBlank challenge: String): OAuth2ConsentRequest
 
     // accepting login request more than once throws HTTP 409 - Conflict
     @PUT
     @Path("/oauth2/auth/requests/login/accept")
     fun acceptLoginRequest(
         @QueryParam("login_challenge") @NotBlank challenge: String,
-        acceptLoginRequest: AcceptLoginRequest,
-    ): RedirectResponse
+        acceptLoginRequest: AcceptOAuth2LoginRequest,
+    ): OAuth2RedirectTo
 
     @PUT
     @Path("/oauth2/auth/requests/login/reject")
     fun rejectLoginRequest(
         @QueryParam("login_challenge") @NotBlank challenge: String,
-        error: GenericError,
-    ): RedirectResponse
+        payload: RejectOAuth2Request,
+    ): OAuth2RedirectTo
 
     // accepting consent more than once does not cause an error
     @PUT
     @Path("/oauth2/auth/requests/consent/accept")
     fun acceptConsentRequest(
         @QueryParam("consent_challenge") @NotBlank challenge: String,
-        acceptConsentRequest: AcceptConsentRequest,
-    ): RedirectResponse
+        acceptConsentRequest: AcceptOAuth2ConsentRequest,
+    ): OAuth2RedirectTo
 
     // rejecting consent more than once does not cause an error
     @PUT
     @Path("/oauth2/auth/requests/consent/reject")
     fun rejectConsentRequest(
         @QueryParam("consent_challenge") @NotBlank challenge: String,
-        error: GenericError,
-    ): RedirectResponse
+        error: RejectOAuth2Request,
+    ): OAuth2RedirectTo
 
     @DELETE
     @Path("/oauth2/auth/sessions/consent")
@@ -92,7 +93,7 @@ interface HydraClient {
     fun introspectToken(
         @FormParam("token") @NotBlank token: String,
         @FormParam("scope") scope: String?,
-    ): OAuth2TokenIntrospection
+    ): IntrospectedOAuth2Token
 }
 
 class GoneException(override val message: String?) : RuntimeException(message)
