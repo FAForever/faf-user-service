3adf92b9f6de8f04cd884220ebb29fa003c21d3f
Sheikah45 <Sheikah450@gmail.com>
2026-09-20 13:42:17 -0400

    Make deletion messages persistent and updaate exchange name

--- src/main/kotlin/com/faforever/userservice/backend/account/AccountDeletionEventPublisher.kt
+++ src/main/kotlin/com/faforever/userservice/backend/account/AccountDeletionEventPublisher.kt
@@ -1,9 +1,13 @@
 package com.faforever.userservice.backend.account
 
+import io.smallrye.reactive.messaging.rabbitmq.OutgoingRabbitMQMetadata
 import jakarta.enterprise.context.ApplicationScoped
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
+import org.eclipse.microprofile.reactive.messaging.Message
+import org.eclipse.microprofile.reactive.messaging.Metadata
 import org.slf4j.LoggerFactory
+import java.util.concurrent.CompletableFuture
 
 @ApplicationScoped
 class AccountDeletionEventPublisher(
@@ -21,6 +25,21 @@ class AccountDeletionEventPublisher(
             event.occurredAt,
         )
 
-        emitter.send(event).toCompletableFuture().join()
+
+        val future = CompletableFuture<Void?>()
+
+        val message = Message.of(
+            event, Metadata.of(OutgoingRabbitMQMetadata.builder().withDeliveryMode(2).build()),
+            {
+                future.complete(null)
+                CompletableFuture.completedFuture(null)
+            }, { throwable ->
+                future.completeExceptionally(throwable)
+                CompletableFuture.completedFuture(null)
+            })
+
+        emitter.send(message)
+
+        future.join()
     }
 }
